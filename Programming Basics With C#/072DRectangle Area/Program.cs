36935354e3f551fa12562760e2d9752975559a52
Viktor Draganov <swifrorlilko@gmail.com>
2019-03-21 13:40:08 +0200

    Add files via upload

---
+++ Programming Basics With C#/072DRectangle Area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _072DRectangle_Area
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+            var width = Math.Abs(x1 - x2);
+            var height = Math.Abs(y1 - y2);
+            var area = width * height;
+            var perimeter = 2 * (width + height);
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+        }
+    }
+}
