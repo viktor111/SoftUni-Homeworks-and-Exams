6711f90d7b1a2c9adcf2e9e18e0941a2177f5cd5
Viktor Draganov <swifrorlilko@gmail.com>
2019-03-27 22:41:14 +0200

    Add files via upload

---
+++ Technology Fundamentals With C#/Associative Arrays - Exercise/01Count Chars in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01Count_Chars_in_a_String
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            char[] text = Console.ReadLine()
+                .Where(x => x != ' ')
+                .ToArray();
+
+            var countChars = new Dictionary<char, int>();
+
+            foreach (var charechter in text)
+            {
+                if (!countChars.ContainsKey(charechter))
+                {
+                    countChars[charechter] = 0;
+                }
+                countChars[charechter]++;
+            }
+
+            foreach (var kvp in countChars)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals With C#/Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var resourceAndQuantity = new Dictionary<string, int>();
+
+            string input = string.Empty;
+            int quantity = 0;
+
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                quantity = int.Parse(Console.ReadLine());
+
+                if (!resourceAndQuantity.ContainsKey(input))
+                {
+                    resourceAndQuantity[input] = quantity;
+                }
+                else
+                {
+                    resourceAndQuantity[input] += quantity;
+                }
+            }
+
+            foreach (var kvp in resourceAndQuantity)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
